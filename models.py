1a10e7d082fe4d2c0413d380cb1605a69df5c2c1
MikeIV2007 <130536460+MikeIV2007@users.noreply.github.com>
2023-10-13 11:02:52 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,18 @@
+from mongoengine import *
+
+from mongoengine.fields import ListField, StringField, ReferenceField
+
+    
+class Author(Document):
+    fullname = StringField(required=True)
+    born_date = StringField(max_length=50)
+    born_location = StringField()
+    description = StringField()
+    meta = {'allow_inheritance': True}
+
+
+class Quote(Document):
+    tags = ListField(StringField()) # list of fields 
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE) # (foregin key)
+    quote = StringField(required=True)
+    meta = {'allow_inheritance': True}
